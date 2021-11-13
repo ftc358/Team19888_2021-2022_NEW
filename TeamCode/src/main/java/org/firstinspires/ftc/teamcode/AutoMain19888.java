8ba12ad3ef91033a6b846157b472b933f3c97fe6
Dannywzx <93743182+Dannywzx@users.noreply.github.com>
2021-11-14 00:03:09 +0800

    1988 Java code reright

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMain19888.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMain19888.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Math.sqrt;
 
 /*
 
@@ -13,41 +16,19 @@ Most of the code is not complete yet.
 
  */
 
-@Autonomous
-public class AutoMain19888 extends LinearOpMode {
-    private DcMotor motorLeft;
-    private DcMotor motorRight;
-    private DcMotor motorWheel;
-    private DcMotor motorSlide;
-    private Servo servoClaw;
-
-    public void runOpMode() throws InterruptedException {
-        motorLeft = hardwareMap.dcMotor.get("motor1");
-        motorRight = hardwareMap.dcMotor.get("motor2");
-        motorWheel = hardwareMap.dcMotor.get("motor3");
-        motorSlide = hardwareMap.dcMotor.get("motor4");
-        servoClaw = hardwareMap.servo.get("servo1");
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-
-        }
-    }
-
-    public void goForward(double speed, int distance) {
-
-    }
-
-    public void turnAround(double speed, int angle) {
-
-    }
-
-    public void grabBlock() {
-
-    }
-
-    public void releaseBlock(int level) {
-        
-    }
-}
+//@Autonomous (name = "autonomous 1")
+//public class AutoC358_Red extends AutoEngine358 {
+//    private double POWER = 1;
+//
+//    public void runOpMode() throws InterruptedException {
+//
+//        initialize(new RobotPosition(0, 0, 90));
+//
+//
+//        while (opModeIsActive()) {
+//            forward(POWER, 30);
+//            turnTo(0, POWER);
+//        }
+//    }
+//
+//}
